5be77b770fff0d360765d2194fbb3be8548022d3
luftkluft <luftkluft@gmail.com>
2019-02-28 21:54:05 +0300

    Building/dev: api space

--- app/controllers/v1/todos_controller.rb
+++ app/controllers/v1/todos_controller.rb
@@ -1,10 +1,11 @@
-class TodosController < ApplicationController
+module V1
+  class TodosController < ApplicationController
   before_action :set_todo, only: [:show, :update, :destroy]
 
   # GET /todos
   def index
     # get current user todos
-    @todos = current_user.todos
+    @todos = current_user.todos.paginate(page: params[:page], per_page: 20)
     json_response(@todos)
   end
 
@@ -41,5 +42,6 @@ class TodosController < ApplicationController
 
   def set_todo
     @todo = Todo.find(params[:id])
+  end
   end
 end
